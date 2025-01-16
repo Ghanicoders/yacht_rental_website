12d0df67f38869b8442cd3e79bcf8a6f0b793e0a
Ghanicoders <musarrafghani8@gmail.com>
2025-01-16 17:14:36 +0530

    after add some changes

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -14,6 +14,7 @@
 <li><a href="#"><i class="fa fa-car"></i> Vehicles</a>
 					<ul>
 						<li><a href="post-avehical.php">Post a Vehicle</a></li>
+						<li><a href="manage-vehicles.php">Manage-Vehicles</a></li>
 
 					</ul>
 				</li>
--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-2">
     <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="assets/pics/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      <img src="assets\images\pics\logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
       Yachts-1
     </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
