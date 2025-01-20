9c7a5973c612729c695ef733f495551460e6f66e
Ghanicoders <musarrafghani8@gmail.com>
2025-01-20 12:15:27 +0530

    now the registration modal is working fine

--- includes/login.php
+++ includes/login.php
@@ -24,42 +24,70 @@ echo "<script type='text/javascript'> document.location = '$currentpage'; </scri
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="modal fade" id="loginform">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h3 class="modal-title">Login</h3>
-      </div>
-      <div class="modal-body">
-        <div class="row">
-          <div class="login_wrap">
-            <div class="col-md-12 col-sm-6">
-              <form method="post">
-                <div class="form-group">
-                  <input type="email" class="form-control" name="email" placeholder="Email address*">
-                </div>
-                <div class="form-group">
-                  <input type="password" class="form-control" name="password" placeholder="Password*">
-                </div>
-                <div class="form-group checkbox">
-                  <input type="checkbox" id="remember">
-               
-                </div>
-                <div class="form-group">
-                  <input type="submit" name="login" value="Login" class="btn btn-block">
-                </div>
-              </form>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Page</title>
+  
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  
+</head>
+
+<body>
+  
+  <!-- Your content goes here (for example, the login modal) -->
+  <div class="modal fade" id="loginform">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h3 class="modal-title">Login</h3>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="login_wrap">
+              <div class="col-md-12 col-sm-6">
+                <form method="post">
+                  <div class="form-group">
+                    <input type="email" class="form-control" name="email" placeholder="Email address*">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control" name="password" placeholder="Password*">
+                  </div>
+                  <div class="form-group checkbox">
+                    <input type="checkbox" id="remember">
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" name="login" value="Login" class="btn btn-block">
+                  </div>
+                </form>
+              </div>
             </div>
-           
           </div>
         </div>
-      </div>
-      <div class="modal-footer text-center">
-        <p>Don't have an account? <a href="#signupform" data-toggle="modal" data-dismiss="modal">Signup Here</a></p>
-        <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal">Forgot Password ?</a></p>
+        <div class="modal-footer text-center">
+          <p>Don't have an account? <a href="#signupform" data-toggle="modal" data-dismiss="modal">Signup Here</a></p>
+          <p><a href="#forgotpassword" data-toggle="modal" data-dismiss="modal">Forgot Password ?</a></p>
+        </div>
       </div>
     </div>
   </div>
-</div>
+  
+  <!-- Include the necessary JavaScript libraries -->
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+  <!-- Popper.js (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+
+  <!-- Bootstrap JS -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  
+
+</body>
+
+</html>
